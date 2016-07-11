8f14a9f7923b7971ce3b5d470f0be2559d5ae0a6
Shannon <s.byrne2254@gmail.com>
2016-07-10 19:59:20 -0500

    Step 3: Adding Mirage and a Profile Model

--- app/controllers/profile.js
+++ app/controllers/profile.js
@@ -1,6 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  queryParams: ['allowed-here'],
   name: 'Emberita',
   color: 'green',
   someInformation: Ember.computed('name', 'color', function(){
---
+++ mirage/factories/profile.js
@@ -0,0 +1,10 @@
+import { Factory, faker } from 'ember-cli-mirage';
+
+export default Factory.extend({
+  name() {
+    return faker.name.firstName();
+  },
+  color() {
+    return faker.list.random('red', 'yellow', 'blue', 'green', 'purple', 'pink')
+  }
+});
